d206fc7193d29f4d7157d07b64bdec54c30725d2
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2019-05-11 20:21:02 +0200

    Solved some errors in JS.

--- app/static/prompt.js
+++ app/static/prompt.js
@@ -1,6 +1,6 @@
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
-  console.log('Vamos a mostrar el prompt;
+  console.log('Vamos a mostrar el prompt');
   e.preventDefault();
-  prompt();
+  e.prompt();
 });
