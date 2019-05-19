3e4bc21d5e51f93395c4f3d86d2604eabfc6eacd
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2019-05-19 14:49:20 +0200

    Edited model.

--- app/server.py
+++ app/server.py
@@ -11,8 +11,8 @@ from fastai.vision import *
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=10YIiDvR-T1XkGRdpapxDRGHSvoogbplo'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1j90w5PDNegYlKoeoXVJEC19r0dMyDyEL'
+export_file_name = 'exportfinal.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
