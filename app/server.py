ef35a89f70b95e54d8e82c74171c7c7e414d7d9d
WaterKnight1998 <dvdlacallecastillo@gmail.com>
2019-05-19 15:16:31 +0200

    Edited model to Pytorch 1.0.0.

--- app/server.py
+++ app/server.py
@@ -11,8 +11,8 @@ from fastai.vision import *
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1j90w5PDNegYlKoeoXVJEC19r0dMyDyEL'
-export_file_name = 'exportfinal.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1BOgke5T3dmTAjRRFArVCB5kHBXcqlU1d'
+export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
